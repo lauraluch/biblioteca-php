581d557423921874ab00f292472e81523e1a6295
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2022-11-25 16:47:04 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+    <div class="grid-container">
+        <div class="grid-item">
+            <img class="logo" src="logo1.png"/>
+            <p>Facilite.</p>
+        </div>
+        <div class="grid-item">
+            <a class="login" href='signup.php'>Cadastre-se</a>
+            <a class="login" href='login.php'>Entre</a>
+        </div>
+    </div>
+    <footer>
+        <p>Desenvolvido por Laura Luchiari, Luiz Antonio Chiquetano e Eduardo Correa</p>
+        <p>link</p>
+    </footer>
+</body>
+</html>
